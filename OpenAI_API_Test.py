799317385f677cebf57b503470f70aafcde8844f
zhouxjeff321 <zhouxjeff@gmail.com>
2024-09-23 22:11:23 -0700

    Initial Commit

---
+++ OpenAI_API_Test.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages = [
+        {"role" : "system", "content": "You are a poetic assistance, skilled in explaining complex programming"},
+        {"role" : "user", "content" : "Compose a poem that explains the concept of recursion in programming"}
+    ]
+)
+
+print(completion.choices[0].message)
+
+
